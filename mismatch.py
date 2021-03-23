98112d2928339aaa3100e95d7aff142c2a4b14d4
Luoyi3819 <79892062+Luoyi3819@users.noreply.github.com>
2021-03-23 15:54:19 +0800

    Update mismatch.py

--- mismatch.py
+++ mismatch.py
@@ -4,19 +4,22 @@ import cv2
 
 def mismatch(img1_path, img2_path):
   
-  img1 = cv2.imread(img1_path)
-  img2 = cv2.imread(img2_path)
-  
+	img1 = cv2.imread(img1_path)
+	img2 = cv2.imread(img2_path)
+
 	H, W = img1.shape[:2]
 	y = random.randint(0, W/2)   #random choose (x,y) from left top region 
 	x = random.randint(0, H/2)
 	template = img1[x:H//2 , y:W//2]   #cut the region as template
-  
+	
+	template = cv2.subtract(template, template.mean())  #do substraction
+	img2 = cv2.subtract(img2, img2.mean())
+	
 	result = cv2.matchTemplate(img2,template,cv2.TM_CCORR_NORMED)   #*******method********
 	cv2.normalize( result, result, 0, 1, cv2.NORM_MINMAX, -1 )
-  
+
 	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)  #get the max value from R
 	dis = math.sqrt(pow(max_loc[0] - y , 2) + pow(max_loc[1] - x ,2)) 
 	print("distance",dis)
-	mm = dis/(abs(dis) + 1)
+	mm = dis/math.sqrt(pow(H/2, 2) + pow(W/2, 2))
 	return mm
